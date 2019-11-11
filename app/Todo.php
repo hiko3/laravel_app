bc6edbdc40b109864ceebec4c57b93fcfa6b8f3b
hiko <maymay1353@gmail.com>
2019-11-11 07:01:11 +0000

    Todoの論理削除実装

--- app/Todo.php
+++ app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
     // protected $table = 'todos';
 
     // protected $primaryKey = 'id';
@@ -16,6 +18,8 @@ class Todo extends Model
     ];
     // $fillableは書き換えることのできるカラムを指定する
     // 書き換えたくない値のカラムを保護する場合に$fillableを使う
+
+    protected $dates = ['deleted_at'];
 
     public function getByUserId($id) //User_idカラムのIDを取得している関数
     {
